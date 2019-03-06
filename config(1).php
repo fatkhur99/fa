62a49186a6397400d387f7cea37e6610e74075ae
fatkhur99 <47076641+fatkhur99@users.noreply.github.com>
2019-03-06 15:25:23 +0700

    Add files via upload

---
+++ config(1).php
@@ -0,0 +1,34 @@
+<?php
+// edit config sesuai contoh dibawah
+// subscribe Channel Cocentz
+$config = [
+
+  "imei" => [
+
+"xxxxxxxxxxxxxxx",// akun 1
+"xxxxxxxxxxxxxxx",// akun 2
+
+],
+
+  "sign" => [
+"xxxxxxxxxxxxxxx",// akun 1
+"xxxxxxxxxxxxxxx",// akun 2
+
+],
+
+  "token" => [
+
+"xxxxxxxxxxxxxxxx",// akun 1
+"xxxxxxxxxxxxxxxx",// akun 2
+
+],
+
+  "uuid" => [
+
+"xxxxxxxxxxxxxxxx",// akun 1
+"xxxxxxxxxxxxxxxx",// akun 2
+
+],
+  ];
+
+?>
