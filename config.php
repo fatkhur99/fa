0c8921978fa049d620727066b791f4eb3cf44839
fatkhur99 <47076641+fatkhur99@users.noreply.github.com>
2019-03-06 15:50:59 +0700

    Update and rename config(1).php to config.php

--- config.php
+++ config.php
@@ -6,27 +6,23 @@ $config = [
   "imei" => [
 
 "xxxxxxxxxxxxxxx",// akun 1
-"xxxxxxxxxxxxxxx",// akun 2
 
 ],
 
   "sign" => [
 "xxxxxxxxxxxxxxx",// akun 1
-"xxxxxxxxxxxxxxx",// akun 2
 
 ],
 
   "token" => [
 
 "xxxxxxxxxxxxxxxx",// akun 1
-"xxxxxxxxxxxxxxxx",// akun 2
 
 ],
 
   "uuid" => [
 
 "xxxxxxxxxxxxxxxx",// akun 1
-"xxxxxxxxxxxxxxxx",// akun 2
 
 ],
   ];
